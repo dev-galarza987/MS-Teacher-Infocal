558cc1423a2ff5cc88326362469e22c157f6f4d3
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-12-04 17:04:20 -0400

    feat(database): Agregando configuración del TypeORM y credenciales de conexión a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeacherModule } from './teacher/teacher.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TeacherModule],
+  imports: [
+    TeacherModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_LOCAL_HOST'),
+        port: configService.get<number>('DB_LOCAL_PORT'),
+        username: configService.get<string>('DB_LOCAL_USERNAME'),
+        password: configService.get<string>('DB_LOCAL_PASSWORD'),
+        database: configService.get<string>('DB_LOCAL_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
