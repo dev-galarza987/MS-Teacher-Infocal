8fc4cc47a2d23de61b06af02eb43d8dbe0124676
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-12-04 18:22:20 -0400

    feat(config): Agregando tipos del puerto, usando fn outputConsole() y configurando CORS()

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import morgan from 'morgan';
+import cors from 'cors';
+import { outputConsole } from './utils/output';
+
+export type Port = string | number;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT: Port = process.env.PORT || 3000;
+  app.use(
+    cors({
+      origin: '*',
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    }),
+  );
   app.use(morgan('dev'));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT, () => {
+    outputConsole(PORT);
+  });
 }
-bootstrap();
+void bootstrap();
