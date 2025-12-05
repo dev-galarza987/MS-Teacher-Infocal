3df2112dd9f4221c7d2bc0b742892af34a66d79e
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-12-05 10:59:25 -0400

    refactor(controller): refactorizando el controlador de la entidad, formateando el archivo y modificando el endpoint @Post()

--- src/teacher/teacher.controller.ts
+++ src/teacher/teacher.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TeacherService } from './teacher.service';
 import { CreateTeacherDto } from './dto/create-teacher.dto';
 import { UpdateTeacherDto } from './dto/update-teacher.dto';
 
-@Controller('teacher')
+@Controller('teacher/')
 export class TeacherController {
   constructor(private readonly teacherService: TeacherService) {}
 
-  @Post()
-  create(@Body() createTeacherDto: CreateTeacherDto) {
-    return this.teacherService.create(createTeacherDto);
+  @Post('create')
+  create(@Body() createTeacher: CreateTeacherDto) {
+    return this.teacherService.create(createTeacher);
   }
 
   @Get()
