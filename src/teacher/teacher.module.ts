af6cb1f80513cda90643c12b58694d825014fa94
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-12-05 15:41:19 -0400

    refactor(database): Agregando el proveedor de base de datos al providers[...databaseProviders] del TeacherModule

--- src/teacher/teacher.module.ts
+++ src/teacher/teacher.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TeacherService } from './teacher.service';
 import { TeacherController } from './teacher.controller';
+import { databaseProviders } from '../shared/database/database.providers';
 
 @Module({
+  imports: [],
   controllers: [TeacherController],
-  providers: [TeacherService],
+  providers: [TeacherService, ...databaseProviders],
 })
 export class TeacherModule {}
