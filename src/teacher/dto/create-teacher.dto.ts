16dece0df4c0decfd00fad9a85d57b16eb86e492
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-12-05 10:53:52 -0400

    feat(database): Agregando validaciones de datos con class-validator en las propiedades de la entidad

--- src/teacher/dto/create-teacher.dto.ts
+++ src/teacher/dto/create-teacher.dto.ts
@@ -1,8 +1,59 @@
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsPhoneNumber,
+  IsPositive,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateTeacherDto {
+  @IsNumber({}, { message: 'El código debe ser un número' })
+  @IsInt({ message: 'El código debe ser un número entero' })
+  @IsPositive({ message: 'El código debe ser un número positivo' })
+  @IsNotEmpty({ message: 'El código no puede estar vacío' })
   code!: number;
+
+  @IsString({ message: 'El nombre debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El nombre no puede estar vacío' })
+  @MinLength(4, { message: 'El nombre debe tener al menos 4 caracteres' })
+  @MaxLength(100, {
+    message: 'El nombre debe tener como máximo 100 caracteres',
+  })
   name!: string;
+
+  @IsString({ message: 'El apellido debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El apellido no puede estar vacío' })
+  @MinLength(7, { message: 'El apellido debe tener al menos 7 caracteres' })
+  @MaxLength(100, {
+    message: 'El nombre debe tener como máximo 100 caracteres',
+  })
   lastname!: string;
+
+  @IsString({ message: 'El teléfono debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El teléfono no puede estar vacío' })
+  @MinLength(7, { message: 'El teléfono debe tener al menos 7 caracteres' })
+  @MaxLength(8, {
+    message: 'El nombre debe tener como máximo 8 caracteres',
+  })
+  @IsPhoneNumber('BO', {
+    message: 'El teléfono debe ser un número de teléfono válido de Bolivia',
+  })
   phone!: string;
+
+  @IsString({ message: 'El email debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El email no puede estar vacío' })
+  @MinLength(1, { message: 'El email debe tener al menos 1 caracteres' })
   email!: string;
+
+  @IsString({ message: 'La contraseña debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'La contraseña no puede estar vacío' })
+  @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
+    message: 'La contraseña debe contener mayúsculas, minúsculas y números',
+  })
   password!: string;
 }
